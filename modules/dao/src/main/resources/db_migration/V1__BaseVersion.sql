564887f26f2c8eec7e25c1adebda94c9fd4a3c73
laiyijie <449132062@qq.com>
2017-09-24 17:30:52 +0800

    for quick start up a project

---
+++ modules/dao/src/main/resources/db_migration/V1__BaseVersion.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `test_count` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `button_name` varchar(45) NOT NULL,
+  `create_time` bigint(20) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7906 DEFAULT CHARSET=utf8;
---
+++ modules/dao/src/test/java/TestExample.java
@@ -0,0 +1,15 @@
+import org.junit.FixMethodOrder;
+import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration({"classpath:test-context.xml"})
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class TestExample {
+
+
+
+}
