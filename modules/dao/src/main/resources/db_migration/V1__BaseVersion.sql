5f254e0c8b86f2a2dd8b4e923a6304e118535ed4
laiyijie <449132062@qq.com>
2017-09-28 22:14:06 +0800

    add part of the dao

--- modules/dao/src/main/resources/db_migration/V1__BaseVersion.sql
+++ modules/dao/src/main/resources/db_migration/V1__BaseVersion.sql
@@ -4,3 +4,15 @@ CREATE TABLE `test_count` (
   `create_time` bigint(20) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=7906 DEFAULT CHARSET=utf8;
+
+
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'the unique id to identify the user',
+  `phone_number` VARCHAR(45),
+  `wechat_union_id` VARCHAR(45),
+  `wechat_open_id` VARCHAR(45),
+  `register_from` VARCHAR(45),
+  `register_time` BIGINT(20) NOT NULL,
+  PRIMARY KEY (`id`)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8
