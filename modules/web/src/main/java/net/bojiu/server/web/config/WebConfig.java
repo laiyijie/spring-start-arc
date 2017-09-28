a5d56084dda7838823008e191beb367b449d5236
laiyijie <449132062@qq.com>
2017-09-28 08:11:39 +0800

    增加跨域

--- modules/web/src/main/java/net/bojiu/server/web/config/WebConfig.java
+++ modules/web/src/main/java/net/bojiu/server/web/config/WebConfig.java
@@ -1,9 +1,11 @@
 package net.bojiu.server.web.config;
 
 import me.laiyijie.spring.log.web.AccessLogFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
@@ -18,6 +20,11 @@ public class WebConfig extends WebMvcConfigurerAdapter{
     public void addInterceptors(InterceptorRegistry registry) {
 //        registry.addInterceptor(new PermissionInterceptor())
 //                .addPathPatterns("/**");
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
     }
 
     @Bean
